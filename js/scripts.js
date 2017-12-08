dc7016da550f2277c1a9a158c32d4ba3a168eeb5
lexa <keitmark@mail.ru>
2017-12-08 11:55:59 +0300

    trololo

--- js/scripts.js
+++ js/scripts.js
@@ -20,4 +20,35 @@ $(document).ready(function() {
         });
         $('body').css({'overflow-y': 'auto'});
     });
+    
+    
+    $('.new-slider-init').slick({
+      centerMode: true,
+      centerPadding: '30%',
+      slidesToShow: 1,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+    
+    
+    
+    
 });
